dc1a44b9cbe3ba2aa0a9c74fbe192dca47f27ac0
Luke Slater <tinmachin3@gmail.com>
2011-08-16 18:05:43 +0100

    changed to literal notation

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ jsbot = require('./jsbot');
 
 Array.prototype.random = function() {
     return this[Math.floor((Math.random()*this.length))];
-}
+};
 
 ///////////////////////////
 
@@ -20,25 +20,25 @@ var instance = jsbot.createJSBot(name, 'elara.ivixor.net', 6667, function() {
 }.bind(this));
 
 instance.addListener('JOIN', function(data) {
-    if(data['user'] == 'Lamp') {
-        instance.say(data['channel'], db.lampPuns.random());
-    } else if(instance.inChannel(data['channel'])) {
-        instance.say('aisbot', '.karma ' + data['user']);
-        waitingForKarma = data['channel'];
+    if(data.user == 'Lamp') {
+        instance.say(data.channel, db.lampPuns.random());
+    } else if(instance.inChannel(data.channel)) {
+        instance.say('aisbot', '.karma ' + data.user);
+        waitingForKarma = data.channel;
     }
 });
 
 instance.addListener('KICK', function(data) {
-    if(data['kickee'] == name) {
-	instance.join(data['channel']);
+    if(data.kickee == name) {
+	instance.join(data.channel);
     } else {
-      instance.say(data['channel'], data['kickee'] + '--');
+      instance.say(data.channel, data.kickee + '--');
     }
 });
 
 instance.addListener('PRIVMSG', function(data) {
-    if(data['user'] == 'aisbot' && data['channel'] == name && waitingForKarma != false) {
-        var split = data['message'].split(' ');
+    if(data.user == 'aisbot' && data.channel == name && waitingForKarma != false) {
+        var split = data.message.split(' ');
         var target = split[0];
         var karma = split[3];
 
@@ -55,8 +55,8 @@ instance.addListener('PRIVMSG', function(data) {
 });
 
 instance.addListener('PRIVMSG', function(data) { 
-    if(data['user'] == admin && data['channel'] == name) {
-        params = data['message'].split(' ');
+    if(data.user == admin && data.channel == name) {
+        params = data.message.split(' ');
         switch(params[0]) {
             case 'join':
                 instance.join(params[1]);
@@ -84,32 +84,32 @@ instance.addListener('PRIVMSG', function(data) {
 });
 
 instance.addListener('PRIVMSG', function(data) {
-    if(instance.inChannel(data['channel']) && data['message'].startsWith('~')) {
-        var params = data['message'].split(' ');
+    if(instance.inChannel(data.channel) && data.message.startsWith('~')) {
+        var params = data.message.split(' ');
         switch(params[0]) {
             case '~kc':
-                instance.say('aisbot', '.karma ' + data['message'].split(' ')[1]);
-                waitingForKarma = data['channel'];
+                instance.say('aisbot', '.karma ' + data.message.split(' ')[1]);
+                waitingForKarma = data.channel;
                 break;
             case '~qset':
-                var qset = data['message'].match(/~qset ([\d\w\s]*)=(.+)$/);
+                var qset = data.message.match(/~qset ([\d\w\s]*)=(.+)$/);
                 if(qset != null && qset.length >= 3) {
                     db.quotes[qset[1]] = qset[2];
-                    instance.say(data['channel'], 'Quote saved as \'' + qset[1] + '\'');
+                    instance.say(data.channel, 'Quote saved as \'' + qset[1] + '\'');
                     fs.writeFile('db.json', JSON.stringify(db, null, '    '));
                 } else {
-                    instance.say(data['channel'], 'Burn the invalid syntax!');
+                    instance.say(data.channel, 'Burn the invalid syntax!');
                 }
                 break;
             case '~q':
-                var q = data['message'].match(/~q ([\d\w\s]*)/)[1].trim();
-                instance.say(data['channel'], q + ': ' + db.quotes[q]);
+                var q = data.message.match(/~q ([\d\w\s]*)/)[1].trim();
+                instance.say(data.channel, q + ': ' + db.quotes[q]);
                 break;
             case '~lamp':
-                instance.say(data['channel'], db.lampPuns.random());
+                instance.say(data.channel, db.lampPuns.random());
                 break;
 	    case '~rq':
-	        instance.say(data['channel'], Object.values(db.quotes).random());
+	        instance.say(data.channel, Object.values(db.quotes).random());
                 break;
         }
     }
