62d9c65a35575e57d575a1abf70ac0d2b2e35d73
Luke Slater <tinmachin3@gmail.com>
2011-08-22 17:54:12 +0100

    Put user commands in their own file and a lot of work on making the organisation more reasonable.

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var jsbot = require('./jsbot');
 var quote = require('./quotes');
+var userCommands = require('./user');
 
 ///////////////////////////
 
@@ -11,184 +12,117 @@ Array.prototype.random = function() {
 ///////////////////////////
 
 
+this.adminCommands = {
+            'join': function(data, params) {
+                instance.join(params[1]); 
+                instance.say(admin, 'Joined ' + params[1]);
+            },
+
+            'part': function(data, params) {
+                instance.part(params[1]);
+                instance.say(admin);
+            },
+
+            'reload': function(data, params) {
+                instance.say(admin, 'Reloading DB.');
+                try {
+                    db = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+                } catch(err) {
+                    instance.say(admin, 'DB reload failed.');
+                } finally {
+                    instance.say(admin, 'DB Reload successful.');
+                }
+            },
+
+            'say': function(data, params) {
+                var c = params[1];
+                var m = params.slice(2).join(' ');
+                instance.say(c, m);
+            }
+        };
+
 
 ///////////////////////////
+var dbot = Class.create({
+    initialize: function(quotes, userCommands) {
+        this.admin = 'reality';
+        this.waitingForKarma = false;
+        this.name = 'depressionbot';
+        this.db = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+        this.quotes = quotes(this.db.quoteArrs);
+        this.userCommands = userCommands.fetch(this);
 
-var dbot = function(quotes) {
-    var admin = 'reality';
-    var waitingForKarma = false;
-    var name = 'depressionbot';
-    var db = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
-    var quotes = quotes(db.quoteArrs);
+        this.instance = jsbot.createJSBot(this.name, 'elara.ivixor.net', 6667, this, function() {
+            this.instance.join('#realitest');
+        }.bind(this));
 
-    var adminCommands = {
-        'join': function(data, params) {
-            instance.join(params[1]); 
-            instance.say(admin, 'Joined ' + params[1]);
-        },
+        this.instance.addListener(this.userCommands.on, this.userCommands.listener);
 
-        'part': function(data, params) {
-            instance.part(params[1]);
-            instance.say(admin);
-        },
-
-        'reload': function(data, params) {
-            instance.say(admin, 'Reloading DB.');
-            try {
-                db = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
-            } catch(err) {
-                instance.say(admin, 'DB reload failed.');
-            } finally {
-                instance.say(admin, 'DB Reload successful.');
+        this.instance.addListener('JOIN', function(data) {
+            if(data.user == 'Lamp') {
+                this.instance.say(data.channel, db.quoteArrs.lamp.random());
+            } else if(data.user == 'reality') {
+                this.instance.say(data.channel, db.realiPuns.random());
+            } else if(instance.inChannel(data.channel)) {
+                this.instance.say('aisbot', '.karma ' + data.user);
+                this.waitingForKarma = data.channel;
             }
-        },
+        });
 
-        'say': function(data, params) {
-            var c = params[1];
-            var m = params.slice(2).join(' ');
-            instance.say(c, m);
-        }
-    };
-
-    var userCommands = {
-        '~kc': function(data, params) {
-            instance.say('aisbot', '.karma ' + data.message.split(' ')[1]);
-            waitingForKarma = data.channel;
-        },
-
-        '~q': function(data, params) {
-            var q = data.message.match(/~q ([\d\w\s]*)/)
-            if(q != undefined) {
-                instance.say(data.channel, quotes.get(q[1].trim()));
-            }
-        },
-
-        '~qadd': function(data, params) {
-            var q = data.message.match(/~qadd ([\d\w\s]*)=(.+)$/);
-            if(q != null && q.length >= 3) {
-                instance.say(data.channel, quotes.add(q));
-                fs.writeFile('db.json', JSON.stringify(db, null, '    '));
+        this.instance.addListener('KICK', function(data) {
+            if(data.kickee == name) {
+                this.instance.join(data.channel);
+                this.instance.say(data.channel, 'Thou shalt not kick ' + name);
+                this.db.kicks[name] += 1;
             } else {
-                instance.say(data.channel, 'Burn the invalid syntax!');
-            }
-        },
-
-        '~qset': function(data, params) {
-            var q = data.message.match(/~qset ([\d\w\s]*)=(.+)$/);
-            if(q != undefined && q.length >= 3) {
-                instance.say(data.channel, quotes.set(q));
-            }
-        },
-
-        '~qcount': function(data, params) {
-            var q = data.message.match(/~qcount ([\d\w\s]*)/)[1].trim();
-            if(q != undefined) {
-                instance.say(data.channel, quotes.count(q));
-            }
-        },
-
-        '~reality': function(data, params) {
-            instance.say(data.channel, db.realiPuns.random());
-        },
-
-        '~d': function(data, params) {
-            instance.say(data.channel,  data.user + ': ' + db.quoteArrs['depressionbot'].random());
-        },
-
-        '~rq': function(data, params) {
-            instance.say(data.channel, quotes.random());
-        },
-
-        '~kickcount': function(data, params) {
-            if(db.kicks[params[1]] == undefined) {
-                instance.say(data.channel, params[1] + ' has either never been kicked or does not exist.');
-            } else {
-                instance.say(data.channel, params[1] + ' has been kicked ' + db.kicks[params[1]] + ' times.');
-            }
-        }
-    };
-
-    var instance = jsbot.createJSBot(name, 'elara.ivixor.net', 6667, function() {
-        instance.join('#realitest');
-        instance.join('#42');
-        instance.join('#itonlygetsworse');
-    }.bind(this));
-
-    instance.addListener('JOIN', function(data) {
-        if(data.user == 'Lamp') {
-            instance.say(data.channel, db.quoteArrs.lamp.random());
-        } else if(data.user == 'reality') {
-            instance.say(data.channel, db.realiPuns.random());
-        } else if(instance.inChannel(data.channel)) {
-            instance.say('aisbot', '.karma ' + data.user);
-            waitingForKarma = data.channel;
-        }
-    });
-
-    instance.addListener('KICK', function(data) {
-        if(data.kickee == name) {
-            instance.join(data.channel);
-            instance.say(data.channel, 'Thou shalt not kick ' + name);
-            db.kicks[name] += 1;
-        } else {
-            if(db.kicks[data.kickee] == undefined) {
-                db.kicks[data.kickee] = 1;
-            } else {
-                db.kicks[data.kickee] += 1;
-            }
-            instance.say(data.channel, data.kickee + '-- (' + data.kickee + ' has been kicked ' + db.kicks[data.kickee] + ' times)');
-        }
-        fs.writeFile('db.json', JSON.stringify(db, null, '    '));
-    });
-
-    instance.addListener('PRIVMSG', function(data) {
-        if(data.user == 'aisbot' && data.channel == name && waitingForKarma != false && data.message.match(/is at/)) {
-            var split = data.message.split(' ');
-            var target = split[0];
-            var karma = split[3];
-
-            if(karma.startsWith('-')) {
-                instance.say(waitingForKarma, target + db.hatedPhrases.random() + ' (' + karma + ')');
-            } else if(karma == '0') {
-                instance.say(waitingForKarma, 'All ' + target + ' knows is that their gut says \'maybe.\' (0)');
-            } else {
-                instance.say(waitingForKarma, target + db.lovedPhrases.random() + ' (' + karma + ')');
+                if(this.db.kicks[data.kickee] == undefined) {
+                    this.db.kicks[data.kickee] = 1;
+                } else {
+                    this.db.kicks[data.kickee] += 1;
+                }
+                instance.say(data.channel, data.kickee + '-- (' + data.kickee + ' has been kicked ' + this.db.kicks[data.kickee] + ' times)');
             }
 
-            waitingForKarma = false;
-        }
-    });
+            this.save();
+        });
 
-    instance.addListener('PRIVMSG', function(data) { 
-        params = data.message.split(' ');
-        if(data.user == admin && data.channel == name && adminCommands[params[0]] != undefined) {
-            adminCommands[params[0]](data, params);
-        } else {
-            if(data.channel == name) data.channel = data.user;
+        this.instance.addListener('PRIVMSG', function(data) {
+            if(data.user == 'aisbot' && data.channel == name && waitingForKarma != false && data.message.match(/is at/)) {
+                var split = data.message.split(' ');
+                var target = split[0];
+                var karma = split[3];
 
-            if(userCommands[params[0]] != undefined) {
-                userCommands[params[0]](data, params);
-            } else {
-                var q = data.message.match(/^~([\d\w\s]*)/)
-                if(q != undefined) {
-                    instance.say(data.channel, quotes.get(q[1].trim()));
+                if(karma.startsWith('-')) {
+                    this.instance.say(waitingForKarma, target + this.db.hatedPhrases.random() + ' (' + karma + ')');
+                } else if(karma == '0') {
+                    this.instance.say(waitingForKarma, target + this.db.neutralPhrases.random() + ' (0)');
+                } else {
+                    this.instance.say(waitingForKarma, target + this.db.lovedPhrases.random() + ' (' + karma + ')');
+                }
+
+                waitingForKarma = false;
+            }
+        });
+
+        this.instance.addListener('PRIVMSG', function(data) {
+            if(data.user == 'reality') {
+                var once = data.message.match(/I ([\d\w\s]* once.)/);
+                if(once != null) {
+                    this.db.realiPuns.push('reality ' + once[1]);
+                    this.instance.say(data.channel, '\'reality ' + once[1] + '\' saved.');
+                    this.save();
                 }
             }
-        }
-    });
+        });
+    },
 
-    instance.addListener('PRIVMSG', function(data) {
-        if(data.user == 'reality') {
-            var once = data.message.match(/I ([\d\w\s]* once.)/);
-            if(once != null) {
-                db.realiPuns.push('reality ' + once[1]);
-                instance.say(data.channel, '\'reality ' + once[1] + '\' saved.');
-                fs.writeFile('db.json', JSON.stringify(db, null, '    '));
-            }
-        }
-    });
+    say: function(channel, data) {
+        this.instance.say(channel, data);
+    },
 
-    return this;
-}(quote.fetch());
+    save: function() {
+        fs.writeFile('db.json', JSON.stringify(this.db, null, '    '));
+    }
+});
 
-
+new dbot(quote.fetch(), userCommands);
---
+++ user.js
@@ -0,0 +1,83 @@
+var userCommands = function(dbot) {
+    var dbot = dbot;
+
+    var commands = {
+        '~kc': function(data, params) {
+            dbot.say('aisbot', '.karma ' + data.message.split(' ')[1]);
+            dbot.waitingForKarma = data.channel;
+        },
+
+        '~q': function(data, params) {
+            var q = data.message.match(/~q ([\d\w\s]*)/)
+            if(q != undefined) {
+                dbot.say(data.channel, dbot.quotes.get(q[1].trim()));
+            }
+        },
+
+        '~qadd': function(data, params) {
+            var q = data.message.match(/~qadd ([\d\w\s]*)=(.+)$/);
+            if(q != null && q.length >= 3) {
+                dbot.say(data.channel, dbot.quotes.add(q));
+                dbot.save();
+            } else {
+                dbot.say(data.channel, 'Burn the invalid syntax!');
+            }
+        },
+
+        '~qset': function(data, params) {
+            var q = data.message.match(/~qset ([\d\w\s]*)=(.+)$/);
+            if(q != undefined && q.length >= 3) {
+                dbot.say(data.channel, dbot.quotes.set(q));
+            }
+        },
+
+        '~qcount': function(data, params) {
+            var q = data.message.match(/~qcount ([\d\w\s]*)/)[1].trim();
+            if(q != undefined) {
+                dbot.say(data.channel, dbot.quotes.count(q));
+            }
+        },
+
+        '~reality': function(data, params) {
+            dbot.say(data.channel, dbot.db.realiPuns.random());
+        },
+
+        '~d': function(data, params) {
+            dbot.say(data.channel,  data.user + ': ' + dbot.db.quoteArrs['depressionbot'].random());
+        },
+
+        '~rq': function(data, params) {
+            dbot.say(data.channel, dbot.quotes.random());
+        },
+
+        '~kickcount': function(data, params) {
+            if(dbot.db.kicks[params[1]] == undefined) {
+                dbot.say(data.channel, params[1] + ' has either never been kicked or does not exist.');
+            } else {
+                dbot.say(data.channel, params[1] + ' has been kicked ' + dbot.db.kicks[params[1]] + ' times.');
+            }
+        }
+    };
+
+    return {
+        'listener': function(data) {
+            params = data.message.split(' ');
+            if(data.channel == dbot.name) data.channel = data.user;
+
+            if(commands.hasOwnProperty(params[0])) {
+                commands[params[0]](data, params);
+            } else {
+                var q = data.message.match(/^~([\d\w\s]*)/)
+                if(q != undefined) {
+                    dbot.say(data.channel, dbot.quotes.get(q[1].trim()));
+                }
+            }
+        },
+
+        'on': 'PRIVMSG'
+    }
+};
+
+exports.fetch = function(dbot) {
+    return userCommands(dbot);
+};
