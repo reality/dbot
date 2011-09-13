8ed3c46c22aad33c41c842cfa7146058c2327827
Luke Slater <reality@mes.localdomain>
2011-09-13 21:24:11 +0100

    add web to run

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ var timers = require('./timer');
 var jsbot = require('./jsbot');
 require('./snippets');
 
-var modules = ['user', 'admin', 'puns', 'kick', 'reality', 'karma', 'youare', 'quotes'];
+var modules = ['user', 'web', 'admin', 'puns', 'kick', 'reality', 'karma', 'youare', 'quotes'];
 
 var DBot = function(dModules, timers) {
     this.admin = 'reality';
