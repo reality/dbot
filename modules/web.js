75899123c9aef20dc6ad027dbea8ab4f0a30f7bf
Luke Slater <reality@mes.localdomain>
2011-09-13 21:55:20 +0100

    remake the app inside the module

--- modules/web.js
+++ modules/web.js
@@ -1,20 +1,20 @@
 var express = require('express');
-var app = express.createServer();
-var pub = '../public';
-
-app.use(express.compiler({ src: pub, enable: ['sass'] }));
-app.use(express.static(pub));
-app.set('view engine', 'jade');
 
 var webInterface = function(dbot) {
     var dbot = dbot;
+
+    var pub = '../public';
+    var app = express.createServer();
+
+    app.use(express.compiler({ src: pub, enable: ['sass'] }));
+    app.use(express.static(pub));
+    app.set('view engine', 'jade');
 
     app.get('/', function(req, res) {
         res.render('index', { });
     });
 
     app.listen(1337);
-    
     return { };
 };
 
