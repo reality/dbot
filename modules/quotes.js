617ce3ca12806eee73abce9322c9980d30b1abf9
Luke Slater <reality@mes.localdomain>
2011-10-06 13:14:56 +0100

    Merge quotes and reality

--- modules/quotes.js
+++ modules/quotes.js
@@ -105,7 +105,25 @@ var quotes = function(dbot) {
                 rmAllowed = true;
             });
             return commands;
-        }
+        },
+
+        // For automatic quote retrieval
+        'listener': function(data, params) {
+            if(data.user == 'reality') {
+                var once = data.message.valMatch(/^I ([\d\w\s,'-]* once)/, 2);
+            } else {
+                var once = data.message.valMatch(/^reality ([\d\w\s,'-]* once)/, 2);
+            }
+
+            if(once) {
+                dbot.db.quoteArrs['realityonce'].push('reality ' + once[1] + '.');
+                dbot.instance.say(data.channel, '\'reality ' + once[1] + '.\' saved.');
+                dbot.save();
+            }
+        },
+
+        'on': 'PRIVMSG'
+
     };
 };
 
