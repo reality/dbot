5a97dea05b3044386673127a4e29917ccdb5682a
Luke Slater <tinmachin3@gmail.com>
2011-10-15 19:54:29 +0100

    modehate only on deop

--- modules/modehate.js
+++ modules/modehate.js
@@ -3,6 +3,7 @@ var modehate = function(dbot) {
 
     return {
         'listener': function(data, params) {
+            console.log(data.raw);
             if(dbot.db.modehate.include(data.user) && data.raw.indexOf('-o') != -1) {
                 dbot.instance.send('KICK #42 ' + data.user + ' :gtfo');
             }
