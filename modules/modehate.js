b6bbab1ba7322d79894e9554fa53e5aa176c88bf
Luke Slater <tinmachin3@gmail.com>
2011-10-16 20:02:27 +0100

    debug

--- modules/modehate.js
+++ modules/modehate.js
@@ -4,6 +4,7 @@ var modehate = function(dbot) {
     return {
         'listener': function(data, params) {
             console.log(typeof(data.raw[0]));
+            console.log(data.raw[0]);
             if(dbot.db.modehate.include(data.user) && data.raw.indexOf('-o') != -1) {
                 dbot.instance.send('KICK #42 ' + data.user + ' :gtfo');
             }
