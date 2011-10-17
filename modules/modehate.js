18906bf94074063fd7a637d57762becfd3db5d43
Luke Slater <tinmachin3@gmail.com>
2011-10-17 13:35:30 +0100

    mass deop protection

--- modules/modehate.js
+++ modules/modehate.js
@@ -3,8 +3,10 @@ var modehate = function(dbot) {
 
     return {
         'listener': function(data, params) {
-            console.log(data.raw[0].indexOf('-o'));
-            if(dbot.db.modehate.include(data.user) && data.raw[0].indexOf('-o') != -1) {
+            // TODO clean this shit up ffs
+            if(data.raw[0].indexOf('-oooo') != -1) {
+                dbot.instance.send('KICK #42 ' + data.user + ' :gtfo - mass deop protection');
+            } else if(dbot.db.modehate.include(data.user) && data.raw[0].indexOf('-o') != -1) {
                 dbot.instance.send('KICK #42 ' + data.user + ' :gtfo');
             }
         },
