b0d82acf77c5071a8154fe716dbe95f85ef62e69
Luke Slater <tinmachin3@gmail.com>
2011-11-17 22:06:56 +0000

    ignore spelling if *word*

--- modules/spelling.js
+++ modules/spelling.js
@@ -4,7 +4,7 @@ var spelling = function(dbot) {
     
     return {
         'listener': function(data, params) {
-            var q = data.message.valMatch(/^\*\*?([\d\w\s]*)/, 2);
+            var q = data.message.valMatch(/^\*\*?([\d\w\s]*)[^*]/, 2);
             var otherQ = data.message.valMatch(/^([\d\w\s]*): \*\*?([\d\w\s]*)/, 3);
             if(q) {
                 var correction = q[1];
