d0b3b5252c17c81ca8eda33d42a5da09677e1b7a
Luke Slater <tinmachin3@gmail.com>
2011-12-04 17:03:33 +0000

    modehate

--- modules/modehate.js
+++ modules/modehate.js
@@ -6,6 +6,7 @@ var modehate = function(dbot) {
             console.log('THIS: ' + data.raw[0]);
             if(data.raw[0].indexOf('-oooo') != -1) {
                 console.log('test');
+                console.log('KICK ' + data.channel + ' ' + data.user + ' :gtfo - mass deop protection');
                 dbot.instance.send('KICK ' + data.channel + ' ' + data.user + ' :gtfo - mass deop protection');
             } else if(dbot.db.modehate.include(data.user) && data.raw[0].indexOf('-o') != -1) {
                 dbot.instance.send('KICK ' + data.channel + ' ' + data.user + ' :gtfo');
