d6eb37b2264421beed19741b172ba806093668a3
Luke Slater <tinmachin3@gmail.com>
2011-12-04 17:05:23 +0000

    quick fix for 42 because its not pulling channel right from jsbot

--- modules/modehate.js
+++ modules/modehate.js
@@ -3,11 +3,8 @@ var modehate = function(dbot) {
 
     return {
         'listener': function(data, params) {
-            console.log('THIS: ' + data.raw[0]);
             if(data.raw[0].indexOf('-oooo') != -1) {
-                console.log('test');
-                console.log('KICK ' + data.channel + ' ' + data.user + ' :gtfo - mass deop protection');
-                dbot.instance.send('KICK ' + data.channel + ' ' + data.user + ' :gtfo - mass deop protection');
+                dbot.instance.send('KICK #42 ' + data.user + ' :gtfo - mass deop protection');
             } else if(dbot.db.modehate.include(data.user) && data.raw[0].indexOf('-o') != -1) {
                 dbot.instance.send('KICK ' + data.channel + ' ' + data.user + ' :gtfo');
             }
