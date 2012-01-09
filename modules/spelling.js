bb934fb1db81354694c676d6d8aac81c736173ad
Luke Slater <tinmachin3@gmail.com>
2012-01-09 14:46:09 +0000

    fix spelling bug with replacing parts of words instead of the word if the letter sequence occurs first

--- modules/spelling.js
+++ modules/spelling.js
@@ -17,7 +17,7 @@ var spelling = function(dbot) {
 
         if(winnerDistance < 7) {
             if(winner !== correction) {
-                var fix = last[data.channel][candidate].replace(winner, correction);
+                var fix = last[data.channel][candidate].replace(' ' + winner + ' ', correction);
                 if (/^.ACTION/.test(fix)) {
                     fix = fix.replace(/^.ACTION/, '/me');
                 }
