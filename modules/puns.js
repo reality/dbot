9284bcc0e9f971dead57f4348610277dc99dcc4e
Luke Slater <reality@turtle.nourishedcloud.com>
2012-04-16 15:23:19 +0100

    add check in puns again derp

--- modules/puns.js
+++ modules/puns.js
@@ -6,7 +6,8 @@ var puns = function(dbot) {
         'listener': function(data) {
             if((dbot.db.ignores.hasOwnProperty(data.user) && 
                         dbot.db.ignores[data.user].include(name)) == false) {
-                if(dbot.moduleNames.include('quotes')) {
+                if(dbot.moduleNames.include('quotes') &&
+                        dbot.db.quotes.hasOwnProperty(data.user)) {
                     data.message = '~q ' + data.user.toLowerCase();
                     var params = data.message.split(' ');
                     dbot.commands[params[0]](data, params);
