82f971040ad0decd1aef679ad45cd1b1102c99af
Luke Slater <tinmachin3@gmail.com>
2012-04-19 12:09:14 +0100

    quoteArrs, not quotes... jesus reality

--- modules/puns.js
+++ modules/puns.js
@@ -7,7 +7,7 @@ var puns = function(dbot) {
             if((dbot.db.ignores.hasOwnProperty(data.user) && 
                         dbot.db.ignores[data.user].include(name)) == false) {
                 if(dbot.moduleNames.include('quotes') &&
-                        dbot.db.quotes.hasOwnProperty(data.user)) {
+                        dbot.db.quoteArrs.hasOwnProperty(data.user)) {
                     data.message = '~q ' + data.user.toLowerCase();
                     var params = data.message.split(' ');
                     dbot.commands[params[0]](data, params);
