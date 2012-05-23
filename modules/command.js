6c7fc838d4f83fff24a7f6d97c5023c96403f401
Luke Slater <tinmachin3@gmail.com>
2012-05-23 15:45:09 +0100

    Apply regex in command.js module and add results in input property on event object.

--- modules/command.js
+++ modules/command.js
@@ -5,12 +5,10 @@
  * ignoring that command.
  */
 var command = function(dbot) {
-    var dbot = dbot;
-
     /**
      * Is user banned from using command?
      */
-    var is_banned = function(user, command) {
+    var isBanned = function(user, command) {
         var banned = false;
         if(dbot.db.bans.hasOwnProperty(command)) {
             if(dbot.db.bans[command].include(user) || dbot.db.bans['*'].include(user)) {
@@ -18,19 +16,38 @@ var command = function(dbot) {
             }
         }
         return banned;
-    }
+    };
 
     /**
      * Is user ignoring command?
      */
-    var is_ignoring = function(user, command) {
+    var isIgnoring = function(user, command) {
         var module = dbot.commandMap[command];
         var ignoring = false;
         if(dbot.db.ignores.hasOwnProperty(user) && dbot.db.ignores[user].include(module)) {
             ignoring = true;
         }
         return ignoring;
-    }
+    };
+
+    /**
+     * Apply Regex to event message, store result. Return false if it doesn't
+     * apply.
+     */
+    var applyRegex = function(commandName, event) {
+        var applies = false;
+        if(dbot.commands[commandName].hasOwnProperty(regex)) {
+            var cRegex = dbot.commands[commandName].regex;
+            var q = event.message.valMatch(cRegex[0], cRegex[1]);
+            if(q) {
+                applies = true;
+                event.input = q;
+            }
+        } else {
+            applies = true;
+        }
+        return applies;
+    };
 
     return {
         'name': 'command',
@@ -39,14 +56,18 @@ var command = function(dbot) {
          * Run the appropriate command given the input.
          */
         'listener': function(event) {
-            var command_name = event.params[0];
-            if(dbot.commands.hasOwnProperty(command_name)) {
-                if(is_banned(event.user, command_name)) {
+            var commandName = event.params[0];
+            if(dbot.commands.hasOwnProperty(commandName)) {
+                if(isBanned(event.user, commandName)) {
                     event.reply(dbot.t('command_ban', {'user': event.user})); 
                 } else {
-                    if(!is_ignoring(event.user, command_name)) {
-                        dbot.commands[command_name](event);
-                        dbot.save();
+                    if(!isIgnoring(event.user, commandName)) {
+                        if(applyRegex(commandName, event)) {
+                            dbot.commands[commandName](event);
+                            dbot.save();
+                        } else {
+                            event.reply(dbot.t('syntax_error'));
+                        }
                     }
                 }
             } 
