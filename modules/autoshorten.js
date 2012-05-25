5677cad1fa008ed9b7a61f90a171535faf49b807
Luke Slater <tinmachin3@gmail.com>
2012-05-25 16:45:47 +0100

    Use jsbot ignore instead of having it build into the modules

--- modules/autoshorten.js
+++ modules/autoshorten.js
@@ -11,28 +11,25 @@ var autoshorten = function(dbot) {
 
     return {
         'listener': function(event) {
-            if((dbot.db.ignores.hasOwnProperty(event.user) && 
-                        dbot.db.ignores[event.user].include(name)) == false) {
-                var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                var urlMatches = event.message.match(urlRegex);
+            var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+            var urlMatches = event.message.match(urlRegex);
 
-                if(urlMatches !== null && urlMatches[0].length > 80) {
-                    var url = urlMatches[0]; // Only doing one, screw you.
-                    
-                    // TODO: Make this use a decent URL shortener. Mine is shit.
-                    var options = {
-                        'host': 'nc.no.de',
-                        'port': 80,
-                        'path': '/mkurl?url=' + escape(url)
-                    };
+            if(urlMatches !== null && urlMatches[0].length > 80) {
+                var url = urlMatches[0]; // Only doing one, screw you.
+                
+                // TODO: Make this use a decent URL shortener. Mine is shit.
+                var options = {
+                    'host': 'nc.no.de',
+                    'port': 80,
+                    'path': '/mkurl?url=' + escape(url)
+                };
 
-                    http.get(options, function(res) {
-                        res.setEncoding('utf8');
-                        res.on('data', function (response) {
-                            event.reply(dbot.t('shorten_link', {'user': event.user}) + JSON.parse(response).surl); 
-                        });
+                http.get(options, function(res) {
+                    res.setEncoding('utf8');
+                    res.on('data', function (response) {
+                        event.reply(dbot.t('shorten_link', {'user': event.user}) + JSON.parse(response).surl); 
                     });
-                }
+                });
             }
         },
 
