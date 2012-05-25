4c72bd327cf03dd416a03e9ba6e848519fee73b1
Luke Slater <tinmachin3@gmail.com>
2012-05-25 16:11:07 +0100

    fixed spelling module and youare

--- modules/youare.js
+++ modules/youare.js
@@ -2,13 +2,13 @@ var youAre = function(dbot) {
     var name = 'youare';
 
     return {
-        'listener': function(data) {
-            if((dbot.db.ignores.hasOwnProperty(data.user) && 
-                        dbot.db.ignores[data.user].include(name)) == false) {
-                var key = data.message.valMatch(/(\bis\b|\bare\b)\s+([\w\s\d]*?)(\s+)?(,|\.|\band\b|$)/, 5);
+        'listener': function(event) {
+            if((dbot.db.ignores.hasOwnProperty(event.user) && 
+                        dbot.db.ignores[event.user].include(name)) == false) {
+                var key = event.message.valMatch(/(\bis\b|\bare\b)\s+([\w\s\d]*?)(\s+)?(,|\.|\band\b|$)/, 5);
 
-                if(key && key[2] != "" && Number.prototype.chanceIn(1, 100) && data.user != 'aisbot') {
-                    dbot.say(data.channel, data.user + ': You\'re ' + key[2] + '.');
+                if(key && key[2] != "" && Number.prototype.chanceIn(1, 100) && event.user != 'aisbot') {
+                    event.reply(event.user + ': You\'re ' + key[2] + '.');
                 }
             }
         },
