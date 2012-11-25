fc83487a5d2edcb3978753a82f7ba89f9a0eca60
Luke Slater <tinmachin3@gmail.com>
2012-11-25 17:56:30 +0000

    Addition to the web interface to view currently connected users and interpolate some data. Little messy atm.

--- modules/web.js
+++ modules/web.js
@@ -9,10 +9,65 @@ var webInterface = function(dbot) {
     app.set('view engine', 'jade');
 
     app.get('/', function(req, res) {
-        //res.redirect('/quotes');
         res.render('index', { 'name': dbot.name });
     });
-    
+
+    app.get('/connections', function(req, res) {
+        var connections = Object.keys(dbot.instance.connections);
+        res.render('connections', { 'name': dbot.name, 'connections': connections });
+    });
+
+    app.get('/channels/:connection', function(req, res) {
+        var connection = req.params.connection;
+        if(dbot.instance.connections.hasOwnProperty(connection)) {
+            var channels = Object.keys(dbot.instance.connections[connection].channels);
+            res.render('channels', { 'name': dbot.name, 'connection': connection, 'channels': channels});
+        } else {
+            res.render('error', { 'name': dbot.name, 'message': 'No such connection.' });
+        }
+    });
+
+    app.get('/users/:connection/:channel', function(req, res) {
+        var connection = req.params.connection;
+        var channel = '#' + req.params.channel;
+        var connections = dbot.instance.connections;
+
+        if(connections.hasOwnProperty(connection) && connections[connection].channels.hasOwnProperty(channel)) {
+            var nicks = connections[connection].channels[channel].nicks;
+            res.render('users', { 'name': dbot.name, 'connection': connection,
+                'channel': channel, 'nicks': nicks });
+        } else {
+            res.render('error', { 'name': dbot.name, 'message': 'No such connection or channel.' });
+        }
+    });
+ 
+    app.get('/user/:connection/:channel/:user', function(req, res) {
+        var connection = req.params.connection;
+        var channel = '#' + req.params.channel;
+        var user = dbot.cleanNick(req.params.user);
+
+        var quoteCount = 'no';
+        if(dbot.db.quoteArrs.hasOwnProperty(user)) {
+            var quoteCount = dbot.db.quoteArrs[user].length;
+        }
+
+        if(!dbot.db.kicks.hasOwnProperty(user)) {
+            var kicks = '0';
+        } else {
+            var kicks = dbot.db.kicks[user];
+        }
+
+        if(!dbot.db.kickers.hasOwnProperty(user)) {
+            var kicked = '0';
+        } else {
+            var kicked = dbot.db.kickers[user];
+        }
+
+        res.render('user', { 'name': dbot.name, 'user': req.params.user,
+        'channel': channel, 'connection': connection, 'cleanUser': user, 
+        'quotecount': quoteCount, 'kicks': kicks, 'kicked': kicked });
+    });
+   
     // Lists the quote categories
     app.get('/quotes', function(req, res) {
         res.render('quotelist', { 'name': dbot.name, 'quotelist': Object.keys(dbot.db.quoteArrs) });
@@ -42,7 +97,7 @@ var webInterface = function(dbot) {
     // Shows the results of a poll
     app.get('/polls/:key', function(req, res) {
         var key = req.params.key.toLowerCase();
-        if(dbot.db.polls.hasOwnProperty(key)) {
+        if(dbot.db.polls.hasOwnProperty(key) && dbot.db.polls[key].hasOwnProperty('description')) {
             // tally the votes
             var totalVotes = 0;
             for( var v in dbot.db.polls[key].votes ) {
