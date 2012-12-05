757344d580d71fd3b7bd475e8a12ac41d0c94c51
Sam Nicholls <sam.n@studio8media.co.uk>
2012-12-05 19:05:19 +0000

    Attempt to fix Issue #54

--- modules/quotes.js
+++ modules/quotes.js
@@ -186,7 +186,7 @@ var quotes = function(dbot) {
         '~link': function(event) {
             var key = event.params[1].trim().toLowerCase();
             if(quotes.hasOwnProperty(key)) {
-                event.reply(dbot.t('quote_link', {'category': key}) + ' - http://nc.no.de:443/quotes/' + key);
+                event.reply(dbot.t('quote_link', {'category': key}) + ' - ' dbot.webUrl + ':' dbot.webPort + '/quotes/' + key);
             } else {
                 event.reply(dbot.t('category_not_found'));
             }
