b2ea634d8cc7d612570b147da993c75d2ba7c43d
Luke Slater <tinmachin3@gmail.com>
2012-12-11 16:04:52 +0000

    Move all the modules to their own directories

---
+++ modules/puns/puns.js
@@ -0,0 +1,25 @@
+var puns = function(dbot) {
+    var name = 'puns';
+    var dbot = dbot;
+
+    return {
+        'name': name,
+        'ignorable': true,
+
+        'listener': function(event) {
+            event.user = dbot.cleanNick(event.user);
+            if(dbot.moduleNames.include('quotes') &&
+                    dbot.db.quoteArrs.hasOwnProperty(event.user)) {
+                event.message = '~q ' + event.user;
+                event.action = 'PRIVMSG';
+                event.params = event.message.split(' ');
+                dbot.instance.emit(event);
+            }
+        },
+        'on': 'JOIN'
+    };
+}
+
+exports.fetch = function(dbot) {
+    return puns(dbot);
+};
