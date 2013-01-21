1958db6df1a6f5e8fc1a19d525d7750d8aa4b388
reality <tinmachin3@gmail.com>
2013-01-21 19:07:25 +0000

    Denting quotes optional [#181]

--- modules/dent/dent.js
+++ modules/dent/dent.js
@@ -34,12 +34,10 @@ var dent = function(dbot) {
     this.commands['~dent'].regex = [/^~dent (.+)$/, 2];
 
     this.onLoad = function() {
-        dbot.api.command.addHook('~qadd', function(key, text) {
-            this.api.post(key + ': ' + text); 
-        }.bind(this));
-
-        if(_.has(dbot.modules, 'quotes')) {
-
+        if(dbot.config.dent.dentQuotes === true) {
+            dbot.api.command.addHook('~qadd', function(key, text) {
+                this.api.post(key + ': ' + text); 
+            }.bind(this));
         }
     }.bind(this);
 };
