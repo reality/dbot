400d215734ef3076a246bf49d64650cae2523b72
reality <tinmachin3@gmail.com>
2013-01-21 19:57:45 +0000

    Correct other people [#154]

--- modules/regex/regex.js
+++ modules/regex/regex.js
@@ -4,11 +4,18 @@ var regex = function(dbot) {
     this.last = {};
     this.listener = function(event) {
         var q = event.message.valMatch(/^s\/(.+)\/(.+)\/$/, 3);
+        var otherQ = event.message.valMatch(/^([\d\w\s]*): s\/(.+)\/(.+)\/$/, 4);
         if(q) {
             var toMatch = new RegExp(q[1]);
             var replaceWith = q[2];
             var last = this.last[event.channel.name][event.user];
             event.reply(event.user + " meant: " + last.replace(toMatch, replaceWith));
+        } else if(otherQ) {
+            var user = otherQ[1]; 
+            var toMatch = new RegExp(otherQ[2]);
+            var replaceWith = otherQ[3];
+            var last = this.last[event.channel.name][user];
+            event.reply(event.user + " thinks " + user + " meant: " + last.replace(toMatch, replaceWith));
         } else {
             if(_.has(this.last, event.channel.name)) {
                this.last[event.channel.name][event.user] = event.message; 
