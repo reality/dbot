612171d28837d2a3613c02cd9ac29d62801330e1
Sam Nicholls <sam.n@studio8media.co.uk>
2013-01-22 20:44:45 +0000

    Preliminary Profiles [#183][#184][MADE BY @samstudio8 #185]

---
+++ modules/profile/pages.js
@@ -0,0 +1,30 @@
+var pages = function(dbot) {
+    var _ = require('underscore')._;
+    var connections = dbot.instance.connections;
+
+    return {
+        '/profile/:connection/:user': function(req, res) {
+            var connection = req.params.connection;
+            var user = dbot.cleanNick(req.params.user);
+
+            var primary = dbot.api.users.resolveUser(connection, user, true);
+            //var profile = dbot.api.profile.getProfile(primary);
+            var profile = dbot.db.profiles[connection][primary.toLowerCase()].profile;
+            var stats = dbot.api.stats.getUserChansStats(connection, primary.toLowerCase(), [
+                    "lines", "words", "lincent", "wpl", "in_mentions"]
+            );
+
+            res.render('profile', {
+                'name': dbot.config.name,
+                'connection': connection,
+                'primary': primary,
+                'profile': profile,
+                'stats': stats.channels,
+            });
+        }
+    }
+};
+
+exports.fetch = function(dbot) {
+    return pages(dbot);
+};
