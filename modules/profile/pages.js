f03dfd91118ade9a131fe829f743a38479746c1d
Thomas Menari <t@menari.eu>
2013-01-25 00:30:09 +0000

    sort photos by primary

--- modules/profile/pages.js
+++ modules/profile/pages.js
@@ -27,9 +27,23 @@ var pages = function(dbot) {
             var connection = req.params.connection;
             var profiles = dbot.db.profiles[connection];
 
+            var nicks = [];
+            for (var p in profiles) {
+              if (profiles.hasOwnProperty(p) && profiles[p].profile.avatar) {
+                nicks.push(p);
+              }
+            }
+            nicks.sort(function(a, b) {
+              var x = profiles[a].profile.primary.toLowerCase();
+              var y = profiles[b].profile.primary.toLowerCase();
+              if(x > y) return 1;
+              if(x < y) return -1;
+              return 0;
+            });
             res.render('profile_grid', {
                 'name': dbot.config.name,
                 'connection': connection,
+                'nicks': nicks,
                 'profiles': profiles,
             });
         }
