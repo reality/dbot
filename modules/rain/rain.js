0e9827a2fc18e9d482f3032333c726e62b6adf5b
Douglas Gardner <douglas@chippy.ch>
2013-01-30 12:38:27 +0000

    further development was precipitated

--- modules/rain/rain.js
+++ modules/rain/rain.js
@@ -9,11 +9,20 @@ var rain = function(dbot) {
     var commands = {
         '~rain': function(event) {
             var apikey = dbot.config.rain.apikey;
-            var precip = event.params[1];
-            var score = 2 * Math.pow(precip,0.5); 
-            score = Math.ceil(score);
-            if (score > 10) { score = 11; }
-            event.reply(dbot.t("rain-"+score));
+            var place = "Aberystwyth"; // you probably need to change the formulae if you change location
+            var url = "http://api.wunderground.com/api/" + apikey + "/conditions/q/CA/" + place + ".json";
+            request(url, function(error, response, body) {
+                if(response.statusCode == "200") {
+                    var data = JSON.parse(body);
+                    var precip = data["precip_1hr_metric"];
+                    var score = 2 * Math.pow(precip,0.5); 
+                    score = Math.ceil(score);
+                    if (score > 10) { score = 11; }
+                } else {
+                    var score = "e";
+                }
+                event.reply(dbot.t("rain-"+score+"[ " + score + " ]"));
+            });
         }
     };
 
