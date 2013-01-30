c9161c10ae6cdd99b129cbbe3487b08da291ab5e
Douglas Gardner <douglas@chippy.ch>
2013-01-30 12:43:17 +0000

    it was raining cats and dogs; there were poodles all over the road

--- modules/rain/rain.js
+++ modules/rain/rain.js
@@ -14,14 +14,14 @@ var rain = function(dbot) {
             request(url, function(error, response, body) {
                 if(response.statusCode == "200") {
                     var data = JSON.parse(body);
-                    var precip = data["precip_1hr_metric"];
+                    var precip = data["current_observation"]["precip_1hr_metric"];
                     var score = 2 * Math.pow(precip,0.5); 
                     score = Math.ceil(score);
                     if (score > 10) { score = 11; }
                 } else {
                     var score = "e";
                 }
-                event.reply(dbot.t("rain-"+score+"[ " + score + " ]"));
+                event.reply(dbot.t("rain-"+score));
             });
         }
     };
