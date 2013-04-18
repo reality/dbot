d3f8e295beff8a14a2b92ce67d7967398e122208
Douglas Gardner <douglas@chippy.ch>
2013-04-18 11:45:15 +0000

    convert to dbot.t; add Italian; add debugMode

--- modules/project/pages.js
+++ modules/project/pages.js
@@ -1,39 +1,29 @@
 var exec = require('child_process').exec;
 
 var pages = function(dbot) {
-    var lang = dbot.config.language;
-    /*
-    var modules = function() {
-        var modlist = dbot.config.moduleNames;
-        var formatted = "<ul>"
-        for (var i = 0, i < modlist.length; i++) {
-            formatted += "<li>" + modlist[i] + "</li>;
-        }
-        formatted += "</ul>"
-        return formatted;
-    } */
     var rev;
     exec("git rev-list --all | wc -l", function(a,b,c){rev = b});
     return {
         '/project': function(req, res) {
             res.render('project', {
                 "name": dbot.config.name,
-                "curr839": lang,
+                "curr839": dbot.config.language,
                 "currver": dbot.config.version,
-                "currlang": dbot.strings["dbotspeaks"][lang].format({ // dbot.t won't work s;
-                    "lang839": lang,
-                    "langen": dbot.strings[lang]["en"],
-                    "lang": dbot.strings[lang][lang],
+                "currlang": dbot.t("dbotspeaks",{
+                    "lang839": dbot.config.language,
+                    "langen": dbot.strings[dbot.config.language]["en"],
+                    "lang": dbot.t(dbot.config.language),
                     "name": dbot.config.name
                 }),
-                "projectstatus": dbot.strings["projectstatus"][lang],
-                "revnum": dbot.strings["revnum"][lang].format({
+                "projectstatus": dbot.t("projectstatus"),
+                "revnum": dbot.t("revnum",{
                     "name": dbot.config.name,
                     "rev": rev,
                     "ver": "abcdef" // TODO, obviously
                 }),
                 "modules": dbot.config.moduleNames,
-                "loadmod": dbot.strings["loadedmodules"][lang]
+                "loadmod": dbot.t("loadedmodules"),
+                "debugmode": dbot.t("debugmode-" + dbot.config.debugMode)
            });
         },
     };
