ab0a110a8ec00dd12d5f24f461379a6196dd4dd9
Sam Nicholls <sam.n@studio8media.co.uk>
2013-04-20 22:51:36 +0100

    Initial databankerisation of profile [#331] * Stats functionality disabled until completion of SamStudio8/dbot-stats#98 * Profile grid currently only works with dbType set to memory * Merge user functionality temporarily disabled

--- modules/profile/profile.js
+++ modules/profile/profile.js
@@ -2,27 +2,14 @@ var _ = require('underscore')._;
 
 var profile = function(dbot) {
 
-    this.profiles = dbot.db.profiles;
-
-    /**
-     * Iterate over known user profiles and ensure they contain all the 
-     * required properties as defined in the configuation.
-     */
     this.onLoad = function(){
         var api = this.api;
         var schema = this.config.schema;
 
-        // Ensure all known users have a profile
-        _.each(dbot.api.users.getAllUsers(), function(server, serverName){
-            _.each(server, function(primary, primaryi){
-                api.createProfile(serverName, primary);
-            });
-        });
-        dbot.save();
-        
         // Add API Hooks
         dbot.api.command.addHook('~setaliasparent', this.api.renameProfile);
-        dbot.api.command.addHook('~mergeusers', this.api.mergeProfile);
+        //TODO(@samstudio8) Profile Merging
+        //dbot.api.command.addHook('~mergeusers', this.api.mergeProfile);
         dbot.api.event.addHook('new_user', this.api.createProfile);
     };
 };
