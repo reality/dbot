086d4be6ab4e3732af6be15a96480f54e866bb08
reality <tinmachin3@gmail.com>
2013-05-17 13:58:44 +0000

    i think adding a this.pages breaks it

--- modules/api/api.js
+++ modules/api/api.js
@@ -5,7 +5,7 @@
 var _ = require('underscore')._;
 
 var api = function(dbot) {
-    this.pages = {
+    /*this.pages = {
         '/api': function(req, res) {
             var externalApi = {};
             _.each(dbot.api, function(moduleApi, moduleName) {
@@ -19,7 +19,7 @@ var api = function(dbot) {
 
             res.render('api', { 'name': dbot.config.name, 'api': externalApi });
         }
-    };
+    };*/
 
     this.onLoad = function() {
         dbot.modules.web.app.get('/api/:module/:method', function(req, res) {
