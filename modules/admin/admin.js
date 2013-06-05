546ce647077bc510f8da1e724e6c9826e955a8cd
reality <tinmachin3@gmail.com>
2013-06-05 21:41:20 +0000

    admin.internalAPI.setConfig operational [#489]

--- modules/admin/admin.js
+++ modules/admin/admin.js
@@ -1,7 +1,7 @@
 /**
  * Module Name: Admin
  * Description: Set of commands which only one who is a DepressionBot
- * administrator can run - as such, it has its own command execution listener.
+ * administrator can run.
  */
 var fs = require('fs'),
     _ = require('underscore')._;
@@ -25,21 +25,26 @@ var admin = function(dbot) {
         },
 
         'setConfig': function(configKey, newOption, callback) {
-            var configPath = dbot.customConfig;
+            var configPath = dbot.customConfig,
+                oldOption = null;
             configKey = configKey.split('.');
 
             for(var i=0;i<configKey.length-1;i++) {
-                if(_.has(configPath, configKey[i])) {
-                    configPath = configPath[configKey[i]];
-                } else {
-                    return;
+                if(!_.has(configPath, configKey[i])) {
+                    configPath[configKey[i]] = {};
                 }
+                configPath = configPath[configKey[i]];
             }
+
+            if(_.has(configPath, configKey[i])) {
+                oldOption = configPath[configKey[i]];
+            }
+            configPath[configKey[i]] = newOption;
             
             this.internalAPI.saveConfig();
             dbot.reloadConfig();
-            callback(true);
-        },
+            callback(null, oldOption);
+        }.bind(this),
 
         'saveConfig': function() {
             var config = dbot.customConfig;
