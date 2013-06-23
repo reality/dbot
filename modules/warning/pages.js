4ffff09381354b033c69a2ac020331957a72beb4
reality <tinmachin3@gmail.com>
2013-06-23 11:37:01 +0000

    Rewrite warning module for databank [#508]

--- modules/warning/pages.js
+++ modules/warning/pages.js
@@ -1,4 +1,5 @@
-var _ = require('underscore')._;
+var _ = require('underscore')._,
+    async = require('async');
 
 var pages = function(dbot) {
     this.warnings = dbot.db.warnings;
@@ -7,37 +8,59 @@ var pages = function(dbot) {
         '/warning': function(req, res) {
             res.render('servers', {  
                 'name': dbot.config.name,
-                'servers': _.keys(this.warnings)
+                'servers': _.keys(dbot.config.servers)
             });
         },
 
         '/warning/:server': function(req, res) {
-            var server = req.params.server;
+            var server = req.params.server,
+                userIds = [],
+                userNicks = [];
 
-            if(_.has(this.warnings, server)) {
-                res.render('users', {
-                    'name': dbot.config.name,
-                    'server': server,
-                    'users': _.keys(this.warnings[server])
+            this.db.search('warnings', { 'server': server }, function(warning) {
+                if(!_.include(userIds, warning.warnee)) userIds.push(warning.warnee);
+            }, function(err) {
+                async.eachSeries(userIds, function(id, callback) {
+                    dbot.api.users.getUser(id, function(user) {
+                        userNicks.push(user.primaryNick); 
+                        callback(false);
+                    });
+                }, function(err) {
+                    res.render('users', {
+                        'name': dbot.config.name,
+                        'server': server,
+                        'users': userNicks
+                    });
                 });
-            } else {
-                res.render('error');
-            }
+            });
         },
 
         '/warning/:server/:user': function(req, res) {
             var server = req.params.server,
                 user = req.params.user;
 
-            if(_.has(this.warnings, server) && _.has(this.warnings[server], user)) {
-                res.render('warnings', {
-                    'name': dbot.config.name,
+            dbot.api.users.resolveUser(server, user, function(user) {
+                var warnings = [];
+                this.db.search('warnings', {
                     'server': server,
-                    'warnings': this.warnings[server][user]
+                    'warnee': user.id
+                }, function(warning) {
+                    warnings.push(warning);
+                }, function(err) {
+                    async.eachSeries(warnings, function(warning, callback) {
+                        dbot.api.users.getUser(warning.warner, function(user) {
+                            warning.warner = user.primaryNick;
+                            callback(false);
+                        });
+                    }, function(err) {
+                        res.render('warnings', {
+                            'name': dbot.config.name,
+                            'server': server,
+                            'warnings': warnings
+                        });
+                    });
                 });
-            } else {
-                res.render('error');
-            }
+            }.bind(this));
         }
     };
 };
