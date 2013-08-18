45eb7785608fe7025ffc0e072e3f4bf57acf7d59
reality <tinmachin3@gmail.com>
2013-08-18 16:39:35 +0000

    derp

--- modules/log/log.js
+++ modules/log/log.js
@@ -7,7 +7,7 @@ var _ = require('underscore')._;
 var log = function(dbot) {
     this.api = {
         'log': function(server, user, message) {
-            var logChannel = this.config.logChannel[event.server];
+            var logChannel = this.config.logChannel[server];
             dbot.say(server, logChannel, dbot.t('log_message', {
                 'time': new Date().toUTCString(),
                 'command': message,
