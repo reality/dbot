1d73115c33cf542918675bf2b61d8faf10cbeb8a
reality <tinmachin3@gmail.com>
2013-08-24 14:46:38 +0000

    search box for the notificatons [close #563]

--- public/script.js
+++ public/script.js
@@ -7,3 +7,15 @@ function search(q)
 		quotes[i].className = quotes[i].innerHTML.indexOf(q) == -1 ? 'quotes hidden' : 'quotes';
 	}
 }
+
+function searchNotifies(q) {
+    var notifies = document.getElementById('notifies');
+    for (var i=1, row; row=notifies.rows[i]; i++) {
+        console.log(row);
+        if(row.cells[3].innerHTML.indexOf(q) == -1) {
+            row.style.display = 'none';
+        } else {
+            row.style.display = '';
+        }
+    }
+}
