5a3b936aff218e89f7e123ae3902dc04cbeff5cf
reality <tinmachin3@gmail.com>
2013-10-20 17:48:58 +0000

    fix the regex

--- modules/users/commands.js
+++ modules/users/commands.js
@@ -197,6 +197,7 @@ var commands = function(dbot) {
     };
     
     commands['~alias'].regex = [/^~alias ([\d\w[\]{}^|\\`_-]+?)/, 2];
+    commands['~time'].regex = [/^~time ([\d\w[\]{}^|\\`_-]+?)/, 2];
     commands['~setaliasparent'].regex = [/^~setaliasparent ([\d\w[\]{}^|\\`_-]+?)/, 2];
     commands['~mergeusers'].regex = [/^~mergeusers ([\d\w[\]{}^|\\`_-]+?)\s*?([\d\w[\]{}^|\\`_-]+?)/, 3];
     commands['~addalias'].regex = [/^~addalias ([\d\w[\]{}^|\\`_-]+?) ([\d\w[\]{}^|\\`_-]+?)$/, 3];
