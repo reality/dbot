1e995d7bbafc46fd70e0a379ecc646897843c365
reality <tinmachin3@gmail.com>
2013-10-23 19:53:14 +0000

    fix profile

--- modules/profile/commands.js
+++ modules/profile/commands.js
@@ -2,7 +2,7 @@ var _ = require('underscore')._;
 
 var commands = function(dbot){
     var commands = {
-        '~getprop': function(event){
+        '~get': function(event){
             if(event.params[1]){
                 if(_.has(this.config.schema.profile, event.params[1])){
                     this.api.getProperty(event.server, event.user, event.params[1], function(reply){
@@ -14,7 +14,7 @@ var commands = function(dbot){
             }
         },
 
-        '~setprop': function(event){
+        '~set': function(event){
             if(event.input[1] && event.input[2]){
                 if(_.has(this.config.schema.profile, event.input[1])){
                     this.api.setProperty(event.server, event.user, event.input[1], event.input[2], function(reply){
@@ -31,7 +31,7 @@ var commands = function(dbot){
             event.reply(dbot.api.web.getUrl('profile/' + event.server + '/' + user));
         }
     };
-    commands['~setprop'].regex = [/~setprop ([^ ]+) (.+)/, 3];
+    commands['~set'].regex = [/~set ([^ ]+) (.+)/, 3];
 
     return commands;
 };
