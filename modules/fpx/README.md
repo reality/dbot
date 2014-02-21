2bdf46c58f6558232a3ea5f7fcd0d22cf2eff958
thoso <thieson08@live.de>
2014-02-21 03:23:45 +0100

    more 500px functionalities like photos by user

--- modules/fpx/README.md
+++ modules/fpx/README.md
@@ -5,6 +5,7 @@ Adds various 500px functionality.
 ### Description
 
 This module provides a command which allows users to search for a random popular 500px photo.
+Users have to set 500px username with ~set [500px username]
 
 ### Dependencies
 
@@ -23,12 +24,21 @@ ignorable and consumerKey has to be configurated. It can be obtained at http://d
 
 ### Commands
 
-
-     ~r500px
+####~r500px
 Responds with a random popular 500px photo.
 Example:
 + ~r500px
 
-### TODO
+####~500px [user]
+Responds with data of a user, if user left blank posters data will be displayed.
+Example:
++ ~500px
++ ~500px reality
 
-Photo by user etc.
+####~last500px [user]
+Responds with the last photo of a user, if user left blank posters last photo will be displayed.
+Example:
++ ~last500px
++ ~last500px reality
+
+### TODO
--- modules/fpx/fpx.js
+++ modules/fpx/fpx.js
@@ -8,6 +8,54 @@ var _ = require('underscore')._,
     API500px = require('500px').API500px;
 
 var fpx = function(dbot) {
+
+	this.internalAPI = {
+	
+		//gets photo by user, returns a JSON, most likely have to use callback
+		'getPhotosByUser' = function(server,nick,callback) {
+		
+			var fpx_username = null;
+			    this.getUser(server,nick,function(user,fpx){
+			        if (fpx==null){
+			            return;
+			        }
+			        else{
+			            fpx_username=fpx;
+			        }
+		        });
+			
+			var username=fpx_username;
+			this.api500px.photos.getByUsername(username,{'sort': 'created_at', 'rpp': '100'},  function(error, results) {
+                if (error) {
+                    event.reply(dbot.t('5px_error'));
+                    console.log(error);
+                } else {
+                    callback(results);
+                }
+            });
+		}
+	
+
+        //callback with (user,fpx_username)
+		'getUser': function(server, nick, callback) {
+            dbot.api.profile.getProfile(server, nick, function(err, user, profile) {
+                if(user) {
+                    if(profile && _.has(profile.profile, '500px')) {
+                        callback(user, profile.profile.500px);
+                    } else {
+                    	event.reply(user.currentNick + ': Set a 500px username with "~set 500px username"');
+                        callback(user, null);
+                    }
+                } else {
+                	event.reply('Unknown user.');
+                    callback(null, null);
+                }
+            });
+        }
+        
+	
+	}
+	
     this.commands = {
         '~r500px': function(event) {
             var random = Math.floor(Math.random() * 30);
@@ -21,11 +69,43 @@ var fpx = function(dbot) {
                     event.reply(dbot.t('5px_result',{'name':name,'id':id}));
                 }
             });
-        }
+        },
+        
+        '~500px': function(event) {
+            var user = event.rUser;
+            if(event.res[0]) {
+                user = event.res[0].user;
+            }
+
+		this.internalAPI.getPhotosByUser(event.server,user, function(results) {
+			var fpx_name=user.currentNick,
+				fpx_photos=results.total_items,
+				fpx_id=results.photos[0].user.username;
+			event.reply(dbot.t('5px_profile',{'name'=fpx_name,'total'=fpx_photos,'fpx_username'=fpx_id}));
+		},
+		
+		'~last500px':function(event){
+		    var user = event.rUser;
+                if(event.res[0]) {
+                    user = event.res[0].user;
+                }
+		    this.internalAPI.getPhotosByUser(event.server,user, function(results) {
+		    var name = results.photos[0].name,
+                id = results.photos[0].id;
+                event.reply(dbot.t('5px_result',{'name':name,'id':id}));		
+		    });
+		}
+		
+		
+		);
+        
+        },
+
     };
     this.onLoad = function() {
         this.api500px = new API500px(this.config.api_key);
     }.bind(this);
+    
 };
 
 exports.fetch = function(dbot) {
