daa5c9ccc2ce1fc95c230624a0e4f4621f5786bd
reality <tinmachin3@gmail.com>
2014-06-19 14:41:57 +0000

    some updates to the rss module

--- modules/rss/rss.js
+++ modules/rss/rss.js
@@ -2,26 +2,29 @@
  * Module Name: RSS
  * Description: Allows to read RSS feeds
  */
-var FeedParser = require('feedparser')
-, request = require('request')
-, _ = require('underscore')._;
+var FeedParser = require('feedparser'), 
+    request = require('request'), 
+    _ = require('underscore')._;
 
 var rss = function(dbot) {
     this.pollInterval = 120000;
-    var self = this;
+
     this.internalAPI = {
         'makeRequest': function(id,feed) {
-            var fid = id;
-            var req = request(feed.url);
-            var feedparser = new FeedParser();
-            //dbot.say(feed.server,feed.channel,"Sending request for feed "+fid+" name "+feed.name+" lP:"+feed.lastPosted);
+            var fid = id,
+                req = request(feed.url),
+                feedparser = new FeedParser();
+
             req.on('error', function (error) {
+                if(dbot.config.debugMode) {
                     dbot.say(feed.server,feed.channel,"RSS: Request for RSS feed got an error: "+error+" Start self-destruct sequence.");
+                }
             });
+
             req.on('response', function (res) {
                 var stream = this;
 
-                if (res.statusCode != 200){
+                if (res.statusCode !== 200){
                     dbot.say(feed.server,feed.channel,"RSS: RSS server returned status code "+res.statusCode+". Bastard.");
                     return;
                 }
@@ -30,13 +33,16 @@ var rss = function(dbot) {
             });
 
             feedparser.on('error', function(error) {
-                dbot.say(feed.server,feed.channel,"RSS: Feedparser encountered an error: "+error+";;; Inform administrator!");
+                if(dbot.config.debugMode) {
+                    dbot.say(feed.server,feed.channel,"RSS: Feedparser encountered an error: "+error+";;; Inform administrator!");
+                }
             });
+
             feedparser.on('readable', function() {
                 // This is where the action is!
-                var stream = this
-                 , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-                 , item;
+                var stream = this, 
+                    meta = this.meta, // **NOTE** the "meta" is always available in the context of the feedparser instance
+                    item;
 
                 while (item = stream.read()) {
                     if(item.pubdate.getTime() - feed.lastPosted > 0) {
@@ -72,16 +78,19 @@ var rss = function(dbot) {
                 dbot.db.feeds[fid] = feed;
             });
         }.bind(this),
+
         'checkFeeds': function() {
             console.log("Checking feeds...");
             for(var i=0;i<dbot.db.feeds.length;++i) {
                 this.internalAPI.makeRequest(i,dbot.db.feeds[i]);
             }
         }.bind(this),
+
         'reloadFeeds': function() {
-            return setInterval(this.internalAPI.checkFeeds,self.pollInterval);
+            return setInterval(this.internalAPI.checkFeeds, this.pollInterval);
         }.bind(this)
     };
+
     this.commands = {
         '~addrssfeed': function(event) {
             if(event.params.length < 3) {
@@ -92,24 +101,27 @@ var rss = function(dbot) {
             dbot.db.feeds.push({server:event.server, channel:event.channel.name, name:event.params[1], url:event.params[2], lastPosted: now, newTime: now});
             event.reply("Adding RSS feed named "+event.params[1]+" with URL "+event.params[2]);
         },
+
         '~rsstest': function(event) {
-            event.reply("I posted RSS last @ "+self.lastPosted);
+            event.reply("I posted RSS last @ "+this.lastPosted);
             event.reply("Nothing to test. Go home.");
         },
+
         '~delrssfeed': function(event) {
             for(var i=0;i<dbot.db.feeds.length;++i) {
                 if(dbot.db.feeds[i].server == event.server && dbot.db.feeds[i].channel == event.channel.name && dbot.db.feeds[i].name == event.params[1]) {
-                    event.reply("Found feed "+event.params[1]+" you were looking for...");
-                    dbot.db.feeds.splice(i,1);
-                    event.reply("... removed!");
+                    dbot.db.feeds.splice(i, 1);
+                    event.reply("Removed feed "+event.params[1]+" you were looking for...");
                     break;
                 }
             }
         }
     };
+
     this.onLoad = function() {
         this.interval = this.internalAPI.reloadFeeds();
     };
+
     this.onDestroy = function() {
         clearInterval(this.interval);
     };
