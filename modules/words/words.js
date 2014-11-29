2d8a1da4702798ee5d99302c95260b5895daa5a2
reality <tinmachin3@gmail.com>
2014-11-29 05:10:34 +0000

    like

--- modules/words/words.js
+++ modules/words/words.js
@@ -65,8 +65,7 @@ var words = function(dbot) {
                 if(!err && word) {
                     this.wn.definitions(encodeURIComponent(word.word), function(err, defs) {
                         if(!err && defs[0]) {
-                        console.log(defs[0].toString());
-                            if(!defs[0].toString().match(/plural/i)) {
+                            if(!String(defs[0]).match(/plural/i)) {
                                 event.reply(dbot.t('def', {
                                     'word': word.word,
                                     'definition': defs[0].text
