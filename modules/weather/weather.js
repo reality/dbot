79a607ba1baa38318c074c6cd27e6d29f1d7be41
reality <tinmachin3@gmail.com>
2015-01-28 07:22:32 +0000

    weather country

--- modules/weather/weather.js
+++ modules/weather/weather.js
@@ -20,7 +20,7 @@ var weather = function(dbot) {
                 'json': true
             }, function(err, response, body) {
                 if(!err && body && _.has(body, 'cod') && body.cod === 200) {
-                    event.reply('['+body.name+']' + ' Condition: ' + body.weather[0].description + ' | Temp: ' + Math.round(body.main.temp) + 'C/'+Math.round(body.main.temp* 9 / 5 + 32)+'F | Humidity: ' + body.main.humidity + '% | Wind Speed: ' + body.wind.speed + 'KM/H');
+                    event.reply('['+body.name+', '+body.sys.country+']' + ' Condition: ' + body.weather[0].description + ' | Temp: ' + Math.round(body.main.temp) + 'C/'+Math.round(body.main.temp* 9 / 5 + 32)+'F | Humidity: ' + body.main.humidity + '% | Wind Speed: ' + body.wind.speed + 'KM/H');
                 } else {
                     event.reply('There is no weather in ' + city);
                 }
