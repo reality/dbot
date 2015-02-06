e9e1e6154077242655f0c681e25948e5855233cb
reality <tinmachin3@gmail.com>
2015-02-06 12:40:15 +0000

    ext quotecat

--- modules/quotes/quotes.js
+++ modules/quotes/quotes.js
@@ -156,6 +156,8 @@ var quotes = function(dbot) {
 
     this.api['getQuote'].external = true;
     this.api['getQuote'].extMap = [ 'key', 'callback' ];
+    this.api['getQuoteCategory'].external = true;
+    this.api['getQuoteCategory'].extMap = [ 'key', 'callback' ];
    
     this.listener = function(event) {
         if(event.action == 'PRIVMSG') {
