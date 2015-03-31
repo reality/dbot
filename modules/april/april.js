db661c1229988c6d8b251bbf3c9b5ddaa022b9f5
Luke Slater <tinmachin3@gmail.com>
2015-04-01 00:42:46 +0300

    Update april.js

--- modules/april/april.js
+++ modules/april/april.js
@@ -1,10 +1,10 @@
 var april = function(dbot) {
     this.listener = function(event) {
         var match = event.message.match(/^i'?( a)?m (an? )?([^ ]+)/i);
-        if(match && event.channel == '#april') {
+        if(match) {
             dbot.say(event.server, 'operserv', 'svsnick ' + event.user + ' ' + match[3]);
             setTimeout(function() {
-                event.reply('Hi ' + match[3] + ', I\'m tripbot!');
+                event.reply('Hi ' + match[3] + ', I\'m ' + dbot.config.name);
             }, 1000);
         }
     }
