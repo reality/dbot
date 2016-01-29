da2ad191c917df45128e45dacc5f7e517ecb0603
reality <tinmachin3@gmail.com>
2016-01-29 22:13:44 +0000

    fix ban count

--- modules/report/commands.js
+++ modules/report/commands.js
@@ -130,7 +130,7 @@ var commands = function(dbot) {
                             items[notify.time] = notify;
                           }
                         }, function() {
-                            if(quiet != 0 || warn != 0 || report != 0) {
+                            if(quiet != 0 || warn != 0 || report != 0 || ban != 0) {
                               event.reply(user.primaryNick + ' has been warned ' + warn + ' times, quieted ' + quiet + ' times, and reported ' + report + ' times.');
 
                               var sTimes = _.keys(items).sort(function(a, b) {
