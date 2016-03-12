fa431ac8006ae4d64e0607e7c4335dd6f8054b2c
reality <tinmachin3@gmail.com>
2016-03-12 22:16:02 +0000

    vitetris

---
+++ modules/vitetris/vitetris.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+    _ = require('underscore')._;
+
+var vitetris = function(dbot) {
+  var lastLine = 0;
+
+  this.onLoad = function() {
+    dbot.api.timers.addTimer(15000, function() {
+      var file = fs.readFileSync(this.config.logFile).toString().split("\n");
+      var newEvents = [];
+
+      if(file.length != lastLine) {
+        newEvents = file.slice(lastLine-1);
+      }
+      lastLine = file.length;
+
+      if(lastLine != 0 && newEvents.length > 0) {
+        _.each(newEvents, function(msg) {
+          var match = msg.match(/([^ ]+) vs\. ([^ ]+) (\d)-(\d)/);
+          if(match) {
+            if(match[3] > match[4]) {
+              dbot.say(this.config.streamServer, this.config.streamChannel, match[1] + ' beat ' + match[2] + ' at tetris ('+match[3] + '-'+match[4]+')');
+            } else {
+              dbot.say(this.config.streamServer, this.config.streamChannel, match[2] + ' beat ' + match[1] + ' at tetris ('+match[3] + '-'+match[4]+')');
+            }
+          }
+        }.bind(this));
+      } 
+    }.bind(this));
+  }.bind(this);
+};
+
+exports.fetch = function(dbot) {
+  return new vitetris(dbot);
+};
