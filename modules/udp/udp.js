e8f657de947ea43cb8abce28943a46d185069fc7
reality <tinmachin3@gmail.com>
2016-05-07 16:29:18 +0000

    that should learn it

--- modules/udp/udp.js
+++ modules/udp/udp.js
@@ -6,20 +6,24 @@
 var dgram = require('dgram');
 
 var udp = function(dbot) {
+  var servers = [];
+
   _.each(dbot.config.modules.udp.servers, function(data) {
     var server = dgram.createSocket("udp4");
+
     server.on("message", function(msg, msginfo) {
         var message = msg.toString();
         if (msginfo.address == data.address) {
             dbot.say(data.server, data.channel, message);
         }
     }.bind(this));
-    this.onLoad = function() {
-      _.each(dbot.config.modules.udp.servers, function(data) {
-        server.bind(data.port);
-      });
-    }.bind(this);
-  }.bind(this));
+  });
+
+  this.onLoad = function() {
+    _.each(servers, function(server) {
+      server.bind(data.port);
+    });
+  }.bind(this);
 };
 
 exports.fetch = function(dbot) {
